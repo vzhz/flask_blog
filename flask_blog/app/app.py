099658e1abd7d4ba8490217ec16dbea28483ca6e
vzhz <vehanus@gmail.com>
2016-10-03 17:37:18 -0400

    add login logout functionality

--- flask_blog/app/app.py
+++ flask_blog/app/app.py
@@ -72,6 +72,37 @@ class FTSEntry(FTSModel):
     class Meta:
         database = database
 
+### Login and logout ###
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        if password == app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('login.html', next_url=next_url)
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('logout.html')
+
 ### Initialization ###
 
 @app.template_filter('clean_querystring')
