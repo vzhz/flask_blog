b4f427f20c046c563313b7a291b3c85eb0114ab8
vzhz <vehanus@gmail.com>
2016-10-03 17:18:22 -0400

    app configuration

---
+++ flask_blog/app/app.py
@@ -0,0 +1,33 @@
+# app.py
+import datetime
+import functools
+import os
+import re
+import urllib
+
+from flask import (Flask, abort, flash, Markup, redirect, render_template,
+                   request, Response, session, url_for)
+from markdown import markdown
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown.extensions.extra import ExtraExtension
+from micawber import bootstrap_basic, parse_html
+from micawber.cache import Cache as OEmbedCache
+from peewee import *
+from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
+from playhouse.sqlite_ext import *
+
+ADMIN_PASSWORD = 'secret'
+APP_DIR = os.path.dirname(os.path.realpath(__file__))
+DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
+DEBUG = False
+SECRET_KEY = 'shhh, secret!'  # Used by Flask to encrypt session cookie.
+SITE_WIDTH = 800
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+flask_db = FlaskDB(app)
+database = flask_db.database
+
+oembed_providers = bootstrap_basic(OEmbedCache())
