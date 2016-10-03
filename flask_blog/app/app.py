da33db2985d82660cf8cad1e93bc770c17e6a5ea
vzhz <vehanus@gmail.com>
2016-10-03 17:24:08 -0400

    define database models

--- flask_blog/app/app.py
+++ flask_blog/app/app.py
@@ -16,6 +16,7 @@ from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
+## TODO at least one-way hash to store the password
 ADMIN_PASSWORD = 'secret'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
@@ -31,3 +32,38 @@ flask_db = FlaskDB(app)
 database = flask_db.database
 
 oembed_providers = bootstrap_basic(OEmbedCache())
+
+class Entry(flask_db.Model):
+    title = CharField()
+    slug = CharField(unique=True)
+    content = TextField()
+    published = BooleanField(index=True)
+    timestamp = DateTimeField(default=datetime.datetime.now, index=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = re.sub('[^\w]+', '-', self.title.lower())
+        ret = super(Entry, self).save(*args, **kwargs)
+
+        # Store search content.
+        self.update_search_index()
+        return ret
+
+    def update_search_index(self):
+        try:
+            fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
+        except FTSEntry.DoesNotExist:
+            fts_entry = FTSEntry(entry_id=self.id)
+            force_insert = True
+        else:
+            force_insert = False
+        fts_entry.content = '\n'.join((self.title, self.content))
+        fts_entry.save(force_insert=force_insert)
+
+
+class FTSEntry(FTSModel):
+    entry_id = IntegerField()
+    content = TextField()
+
+    class Meta:
+        database = database
