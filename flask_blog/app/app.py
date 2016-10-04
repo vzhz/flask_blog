537e5dc89db0f963491a83222eeab1781e0e2106
vzhz <vehanus@gmail.com>
2016-10-03 21:23:09 -0400

    add additional classmethods to Entry class

--- flask_blog/app/app.py
+++ flask_blog/app/app.py
@@ -53,6 +53,30 @@ class Entry(flask_db.Model):
         self.update_search_index()
         return ret
 
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            # Return empty query.
+            return Entry.select().where(Entry.id == 0)
+        else:
+            search = ' '.join(words)
+
+        return (FTSEntry
+                .select(
+                    FTSEntry,
+                    Entry,
+                    FTSEntry.rank().alias('score'))
+                .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
+                .where(
+                    (Entry.published == True) &
+                    (FTSEntry.match(search)))
+                .order_by(SQL('score').desc()))
+
     def update_search_index(self):
         try:
             fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
