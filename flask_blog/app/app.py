10f80726ea992ccfe54dce709fc79b9c5a860550
vzhz <vehanus@gmail.com>
2016-10-05 15:55:08 -0400

    add views for creating and editing entries

--- flask_blog/app/app.py
+++ flask_blog/app/app.py
@@ -163,6 +163,26 @@ def drafts():
     query = Entry.drafts().order_by(Entry.timestamp.desc())
     return object_list('index.html', query)
 
+### GET displays form, POST creates or updates entry ###
+
+@app.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            entry = Entry.create(
+                title=request.form['title'],
+                content=request.form['content'],
+                published=request.form.get('published') or False)
+            flash('Entry created successfully.', 'success')
+            if entry.published:
+                return redirect(url_for('detail', slug=entry.slug))
+            else:
+                return redirect(url_for('edit', slug=entry.slug))
+        else:
+            flash('Title and Content are required.', 'danger')
+    return render_template('create.html')
+
 ### Slug (URL-friendly title) detail ###
 
 @app.route('/<slug>/')
